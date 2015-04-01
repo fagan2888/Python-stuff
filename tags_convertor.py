ce0a1894e1e5dd48d2784eb2cb65637e93cc3c21
meeree <jhazelden@flextech-hs.org>
2015-03-31 20:32:02 -0400

    updated ctags work

--- tags_convertor.py
+++ tags_convertor.py
@@ -1,9 +1,7 @@
 fl = open('/home/jhazelden/py/tags')
 dicts = []
 for line in fl:
-   fields = ["save:"]
-   pyfields = ["py:"]
-   cfields = []
+   line = line.strip()
    if line[0] == "!":
       continue
    else:
@@ -13,24 +11,21 @@ for line in fl:
       search_pattern = words[2]
       dictionary = {"tag": tag_name, "file name": file_name, "search pattern": search_pattern}
       colon = words[3].find(':')
-      period = file_name.find(".")
-      if colon > -1:
-         for i in range(len(words)):
-            if file_name[period:] == ".py":
-               for field in fields:
-                  if words[i].find(field) > -1:
-                     if words[i].find('\n') == -1:
-                        dictionary[field[:-1]] = words[i][colon+1:-1]
-                     else:
-                        dictionary[field[:-1]] = words[i][colon+1:]
-               for field in pyfields:
-                  if words[i].find(field) > -1:
-                     dictionary[pyfield[:-1]] = words[i][colon+1:]
-      else:
-         if words[3].find('\n') == -1:
-            dictionary["kind"] = words[3]
-         else:
-            dictionary["kind"] = words[3][:-1]
+      kind_type = len(words[3])
+      starter = 3
+#      import pdb; pdb.set_trace()
+      if colon == -1 and kind_type == 1:
+         dictionary["kind"] = words[3]
+         starter = 4
+      elif colon == -1 and kind_type > 1:
+         dictionary["KIND"] = words[3]
+         starter = 4
+ 
+      for i in range(starter, len(words)):
+         colon = words[i].find(':')
+         field = words[i][:colon+1]
+         dictionary[field]= words[i][colon+1:]
+
    dicts.append(dictionary)
 print(dicts)
 
